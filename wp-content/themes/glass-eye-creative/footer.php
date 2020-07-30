8415cb7bce0fe03b91fa437a4d4347886ba6aa44
simonojok19 <simonojok19@gmail.com>
2020-07-31 00:13:45 +0300

    added functions php

--- wp-content/themes/glass-eye-creative/footer.php
+++ wp-content/themes/glass-eye-creative/footer.php
@@ -1,6 +1,11 @@
 <footer class="footer">
-	<nav class="footer-navigation">
-
+	<nav>
+		<?php
+		wp_nav_menu($arg = array(
+			"menu_class" => "footer-navigation",
+			"theme_location" => "footer"
+		))
+		?>
 	</nav>
 	<p class="copyright">
 		<small>All content on this site &copy; Glass Eye Creative</small>
--- wp-content/themes/glass-eye-creative/header.php
+++ wp-content/themes/glass-eye-creative/header.php
@@ -11,7 +11,12 @@
 <header class="header">
 	<nav class="main-navigation-container">
 		<a href="index.html"><img src="<?php bloginfo('template_url'); ?>/images/glass-eye-logo.png" alt="" class="logo"></a>
-
+		<?php
+		wp_nav_menu($arg = array(
+			"menu_class" => "main-navigation",
+			"theme_location" => "primary"
+		))
+		?>
 	</nav>
 	<?php wp_head(); ?>
 </header>
