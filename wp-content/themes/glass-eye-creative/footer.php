7b6aae5dcebca3ada52f335697099c02faba8f0a
simonojok19 <simonojok19@gmail.com>
2020-07-31 00:08:07 +0300

    removing

---
+++ wp-content/themes/glass-eye-creative/footer.php
@@ -0,0 +1,13 @@
+<footer class="footer">
+	<nav class="footer-navigation">
+
+	</nav>
+	<p class="copyright">
+		<small>All content on this site &copy; Glass Eye Creative</small>
+	</p>
+	<?php wp_footer(); ?>
+</footer>
+
+</body>
+
+</html>
