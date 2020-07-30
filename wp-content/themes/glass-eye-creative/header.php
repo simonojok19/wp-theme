c6552199a5d0c6b2c5cf10dbdc0eb741502389f2
simonojok19 <simonojok19@gmail.com>
2020-07-31 00:02:28 +0300

    added the file

--- wp-content/themes/glass-eye-creative/header.php
+++ wp-content/themes/glass-eye-creative/header.php
@@ -4,14 +4,14 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Glass Eye Creative: Community Art Center in Oklahoma City | Home</title>
-	<link href="style.css" type="text/css" rel=stylesheet>
+	<link href="<?php bloginfo('template_url'); ?>/style.css" type="text/css" rel=stylesheet>
 	<link href="https://fonts.googleapis.com/css?family=Raleway:300,400,400i,700" rel="stylesheet">
 </head>
 
 <body>
 <header class="header">
 	<nav class="main-navigation-container">
-		<a href="index.html"><img src="images/glass-eye-logo.png" alt="" class="logo"></a>
+		<a href="index.html"><img src="<?php bloginfo('template_url'); ?>/images/glass-eye-logo.png" alt="" class="logo"></a>
 		<ul class="main-navigation">
 			<li><a href="">Home</a></li>
 			<li><a href="">Classes</a></li>
