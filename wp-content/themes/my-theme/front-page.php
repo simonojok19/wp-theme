ee34814e518620d9765ccfd0f5c146602c096a50
simonojok19 <simonojok19@gmail.com>
2020-07-30 13:46:01 +0300

    adding post

--- wp-content/themes/my-theme/front-page.php
+++ wp-content/themes/my-theme/front-page.php
@@ -1,3 +1,15 @@
 <?php get_header(); ?>
 <h1>Front Page</h1>
+<h1><?php the_title(); ?></h1>
+<h1><?php bloginfo('name'); ?></h1>
+<p>
+    <?php
+    if (have_posts()) {
+        while (have_posts()) {
+            the_post();
+            the_content();
+        }
+    }
+    ?>
+</p>
 <?php get_footer(); ?>
