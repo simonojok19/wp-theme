0fc53a4a4e8126720d4cab7081a4e5646deba2b9
simonojok19 <simonojok19@gmail.com>
2020-07-30 23:40:39 +0300

    added the index.php

---
+++ wp-content/themes/glass-eye-creative/index.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Main template file
+ */
+get_header();
+?>
+
+<div class="main-content-width-wrapper">
+	<div class="two-column-entry">
+		<h1>Main Heading</h1>
+
+		<div class="main-content">
+
+		</div>
+	</div>
+</div>
+<?php
+get_footer();
+?>
