9ca97b9d91c5bc6eeeaefe5d1a71f9202994efae
simonojok19 <simonojok19@gmail.com>
2020-07-30 23:46:38 +0300

    finished the index

--- wp-content/themes/glass-eye-creative/index.php
+++ wp-content/themes/glass-eye-creative/index.php
@@ -7,11 +7,19 @@ get_header();
 
 <div class="main-content-width-wrapper">
 	<div class="two-column-entry">
-		<h1>Main Heading</h1>
+		<h1><?php echo get_the_title() ?></h1>
 
-		<div class="main-content">
-
-		</div>
+		<main class="main-content">
+			<?php
+				// start the loop
+				if (have_posts()) {
+					while (have_posts()) {
+						the_post();
+						the_content();
+					}
+				}
+			?>
+		</main>
 	</div>
 </div>
 <?php
