5b9e222fed00f11ff8762e8f7a6240120d89753c
simonojok19 <simonojok19@gmail.com>
2020-07-31 00:27:31 +0300

    added some codes

--- wp-content/themes/glass-eye-creative/functions.php
+++ wp-content/themes/glass-eye-creative/functions.php
@@ -6,3 +6,16 @@ if (!function_exists('glasseye-setup')) {
 }
 
 add_action('after_setup_theme', 'glasseye_setup');
+
+function register_glasseye_menu() {
+	register_nav_menu( array('primary' => __('Primary Menu'), 'footer' => __('footer Menu')));
+}
+
+add_action('init', 'register_glasseye_menu');
+
+function glasseye_scripts() {
+	wp_enqueue_style('glasseye_styles', get_stylesheet_uri());
+	wp_enqueue_style('glasseye_google_fonts', 'https://fonts.googleapis.com/css?family=Raleway:300,400,400i,700');
+}
+
+add_action('wp_enqueue_scripts', 'glasseye_scripts');
