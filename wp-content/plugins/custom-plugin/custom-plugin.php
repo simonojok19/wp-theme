edeadb36b7816ae3ebee68a2a2d6160546dc21d3
simonojok19 <simonojok19@gmail.com>
2020-08-03 15:17:35 +0300

    added some codes

--- wp-content/plugins/custom-plugin/custom-plugin.php
+++ wp-content/plugins/custom-plugin/custom-plugin.php
@@ -1,2 +1,12 @@
 <?php
-
+/**
+ * @package SimonsOjokPlugin
+ * Plugin Name: Custom Plugin
+ * Plugin URI: https://github/simonojok19/wordpress
+ * Description: This plugin was made to demonstrated how to create plugin in wordpress
+ * Version: 1.0.0
+ * Author: Simon Peter Ojok
+ * Author URI: https://github/simonojok19
+ * License: GPLv2 or later
+ * Text Domain: simonojok19
+ */
