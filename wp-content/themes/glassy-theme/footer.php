f3b9d35ab2485121e3d5b1ceced5d676d7331f13
simonojok19 <simonojok19@gmail.com>
2020-08-03 21:24:29 +0300

    removed dynamic link

--- wp-content/themes/glassy-theme/footer.php
+++ wp-content/themes/glassy-theme/footer.php
@@ -1,13 +1,5 @@
 <footer class="footer">
 	<nav class="footer-navigation">
-		<ul>
-			<li><a href="">Home</a></li>
-			<li><a href="">Classes</a></li>
-			<li><a href="">Events</a></li>
-			<li><a href="">Gallery</a></li>
-			<li><a href="">Contact</a></li>
-			<li><a href="">About</a></li>
-		</ul>
 	</nav>
 	<p class="copyright">
 		<small>All content on this site &copy; Glass Eye Creative</small>
