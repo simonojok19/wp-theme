9cac828c3c8a2502965a575ad03c22cf59403869
simonojok19 <simonojok19@gmail.com>
2020-08-03 21:32:19 +0300

    working with menu building

--- wp-content/themes/glassy-theme/footer.php
+++ wp-content/themes/glassy-theme/footer.php
@@ -1,5 +1,11 @@
 <footer class="footer">
-	<nav class="footer-navigation">
+	<nav>
+		<?php
+		wp_nav_menu($arc =array(
+			'menu_class' => 'footer-navigation',
+			'theme_location' => 'footer-nav'
+		))
+		?>
 	</nav>
 	<p class="copyright">
 		<small>All content on this site &copy; Glass Eye Creative</small>
--- wp-content/themes/glassy-theme/header.php
+++ wp-content/themes/glassy-theme/header.php
@@ -12,6 +12,11 @@
 <header class="header">
 	<nav class="main-navigation-container">
 		<a href="index.html"><img src="<?php bloginfo('template_url');?>/images/glass-eye-logo.png" alt="" class="logo"></a>
-
+		<?php
+			wp_nav_menu($arc =array(
+				'menu_class' => 'main-navigation',
+				'theme_location' => 'primary'
+			))
+		?>
 	</nav>
 </header>
