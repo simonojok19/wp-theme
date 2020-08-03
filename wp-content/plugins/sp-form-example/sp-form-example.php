8e598b61de50ad8bdccd0e2db7b2c275ee98fa4e
simonojok19 <simonojok19@gmail.com>
2020-08-03 18:30:44 +0300

    added contact us form

--- wp-content/plugins/sp-form-example/sp-form-example.php
+++ wp-content/plugins/sp-form-example/sp-form-example.php
@@ -1,55 +1,70 @@
 <?php
-/**
- * @package SimonsOjokPlugin
- * Plugin Name: Custom Plugin Form
- * Plugin URI: https://github.com/simonojok19/wp-theme
- * Description: This plugin was made to demonstrated how to create plugin in wordpress
- * Version: 1.0.0
- * Author: Simon Peter Ojok
- * Author URI: https://github.com/simonojok19
- * License: GPLv2 or later
- * Text Domain: simonojok19 0701713575
- *
- * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
- * General Public License version 2, as published by the Free Software Foundation. You may NOT assume
- * that you can use any other version of the GPL.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
- * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- */
+/*
+Plugin Name: Example Contact Form Plugin
+Plugin URI: http://example.com
+Description: Simple non-bloated WordPress Contact Form
+Version: 1.0
+Author: Simon Peter Ojok
+Author URI: http://w3guy.com
+*/
 
 function html_form_code() {
-	echo '
-	<form action="" method="post">
-		<input type="email">
-	</form>
-	';
+	echo '<form action="' . esc_url( $_SERVER['REQUEST_URI'] ) . '" method="post">';
+	echo '<p>';
+	echo 'Your Name (required) <br/>';
+	echo '<input type="text" name="cf-name" pattern="[a-zA-Z0-9 ]+" value="' . ( isset( $_POST["cf-name"] ) ? esc_attr( $_POST["cf-name"] ) : '' ) . '" size="40" />';
+	echo '</p>';
+	echo '<p>';
+	echo 'Your Email (required) <br/>';
+	echo '<input type="email" name="cf-email" value="' . ( isset( $_POST["cf-email"] ) ? esc_attr( $_POST["cf-email"] ) : '' ) . '" size="40" />';
+	echo '</p>';
+	echo '<p>';
+	echo 'Subject (required) <br/>';
+	echo '<input type="text" name="cf-subject" pattern="[a-zA-Z ]+" value="' . ( isset( $_POST["cf-subject"] ) ? esc_attr( $_POST["cf-subject"] ) : '' ) . '" size="40" />';
+	echo '</p>';
+	echo '<p>';
+	echo 'Your Message (required) <br/>';
+	echo '<textarea rows="10" cols="35" name="cf-message">' . ( isset( $_POST["cf-message"] ) ? esc_attr( $_POST["cf-message"] ) : '' ) . '</textarea>';
+	echo '</p>';
+	echo '<p><input type="submit" name="cf-submitted" value="Send"></p>';
+	echo '</form>';
 }
 
-function deliver_email() {
-	if (isset($_POST['cf-submitted'])) {
-		$email = sanitize_email($_POST['cf-email']);
-		$to = get_option('admin_email');
-		$headers = "From: $email";
-		if (wp_mail($to, 'some text', 'message', $headers)) {
-			echo '
-				<div>
-					<p>Thanks the email was sent</p>
-				</div>
-			';
+function deliver_mail() {
+
+	// if the submit button is clicked, send the email
+	if ( isset( $_POST['cf-submitted'] ) ) {
+
+		// sanitize form values
+		$name    = sanitize_text_field( $_POST["cf-name"] );
+		$email   = sanitize_email( $_POST["cf-email"] );
+		$subject = sanitize_text_field( $_POST["cf-subject"] );
+		$message = esc_textarea( $_POST["cf-message"] );
+
+		// get the blog administrator's email address
+		$to = get_option( 'admin_email' );
+
+		$headers = "From: $name <$email>" . "\r\n";
+
+		// If email has been process for sending, display a success message
+		if ( wp_mail( $to, $subject, $message, $headers ) ) {
+			echo '<div>';
+			echo '<p>Thanks for contacting me, expect a response soon.</p>';
+			echo '</div>';
 		} else {
-			echo '
-				<h1>Something went youth</h1>
-			';
+			echo 'An unexpected error occurred';
 		}
 	}
 }
 
 function cf_shortcode() {
 	ob_start();
-	deliver_email();
+	deliver_mail();
 	html_form_code();
+
 	return ob_get_clean();
 }
 
-add_shortcode('sitepoint_contact_form', 'cf_shortcode');
+add_shortcode( 'sitepoint_contact_form', 'cf_shortcode' );
+
+?>
