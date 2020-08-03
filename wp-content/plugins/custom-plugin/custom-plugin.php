95dde4a0714fd88d6e92e8f167b9b842aa298000
simonojok19 <simonojok19@gmail.com>
2020-08-03 15:22:46 +0300

    added the plugin complete

--- wp-content/plugins/custom-plugin/custom-plugin.php
+++ wp-content/plugins/custom-plugin/custom-plugin.php
@@ -9,4 +9,11 @@
  * Author URI: https://github.com/simonojok19
  * License: GPLv2 or later
  * Text Domain: simonojok19
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License version 2, as published by the Free Software Foundation. You may NOT assume
+ * that you can use any other version of the GPL.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
