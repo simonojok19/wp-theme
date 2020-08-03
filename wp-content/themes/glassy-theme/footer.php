c58ed6ad082b8ec41676095559ac536f6afc2324
simonojok19 <simonojok19@gmail.com>
2020-08-03 21:23:43 +0300

    added the footer

---
+++ wp-content/themes/glassy-theme/footer.php
@@ -0,0 +1,20 @@
+<footer class="footer">
+	<nav class="footer-navigation">
+		<ul>
+			<li><a href="">Home</a></li>
+			<li><a href="">Classes</a></li>
+			<li><a href="">Events</a></li>
+			<li><a href="">Gallery</a></li>
+			<li><a href="">Contact</a></li>
+			<li><a href="">About</a></li>
+		</ul>
+	</nav>
+	<p class="copyright">
+		<small>All content on this site &copy; Glass Eye Creative</small>
+	</p>
+	<?php wp_footer() ?>
+</footer>
+
+</body>
+
+</html>
--- wp-content/themes/glassy-theme/header.php
+++ wp-content/themes/glassy-theme/header.php
@@ -3,22 +3,15 @@
 
 <head>
 	<meta charset="UTF-8">
-	<title>Glass Eye Creative: Community Art Center in Oklahoma City | Home</title>
-	<link href="style.css" type="text/css" rel=stylesheet>
+	<link href="<?php bloginfo('template_url');?>/style.css" type="text/css" rel=stylesheet>
 	<link href="https://fonts.googleapis.com/css?family=Raleway:300,400,400i,700" rel="stylesheet">
+	<?php wp_head(); ?>
 </head>
 
 <body>
 <header class="header">
 	<nav class="main-navigation-container">
-		<a href="index.html"><img src="images/glass-eye-logo.png" alt="" class="logo"></a>
-		<ul class="main-navigation">
-			<li><a href="">Home</a></li>
-			<li><a href="">Classes</a></li>
-			<li><a href="">Events</a></li>
-			<li><a href="">Gallery</a></li>
-			<li><a href="">Contact</a></li>
-			<li><a href="">About</a></li>
-		</ul>
+		<a href="index.html"><img src="<?php bloginfo('template_url');?>/images/glass-eye-logo.png" alt="" class="logo"></a>
+
 	</nav>
 </header>
