6a328a5223fbdff090384ce87f689bd85a2aa464
simonojok19 <simonojok19@gmail.com>
2020-08-03 21:07:17 +0300

    added some codes

--- wp-content/themes/glassy-theme/index.php
+++ wp-content/themes/glassy-theme/index.php
@@ -6,20 +6,17 @@ get_header();
 ?>
 <div class="main-content-width-wrapper">
 	<div class="two-column-entry">
-		<h1>Main Heading</h1>
+		<h1><?php echo get_the_title() ?></h1>
 
-		<div class="main-content">
-			<h2>Heading 2</h2>
-			<p>Lorem ipsum dolor sit amet, ei justo partem vivendum eum, ad mel utinam lucilius adolescens. No solet facete perfecto ius, tibique percipitur ei eos. Cu mel nullam suscipiantur, at est link vituperata. Sed ne eius omnis suscipit, vix ex alia assueverit vituperatoribus. At prima maiorum urbanitas pro, modus vocibus noluisse his et.</p>
-			<h3>Heading 3</h3>
-			<p>Lorem ipsum dolor sit amet, ei justo partem vivendum eum, ad mel utinam lucilius adolescens. No solet facete perfecto ius, tibique percipitur ei eos. Cu mel nullam suscipiantur, at est link vituperata. Sed ne eius omnis suscipit, vix ex alia assueverit vituperatoribus. At prima maiorum urbanitas pro, modus vocibus noluisse his et.</p>
-			<h4>Heading 4</h4>
-			<p>Lorem ipsum dolor sit amet, ei justo partem vivendum eum, ad mel utinam lucilius adolescens. No solet facete perfecto ius, tibique percipitur ei eos. Cu mel nullam suscipiantur, at est link vituperata. Sed ne eius omnis suscipit, vix ex alia assueverit vituperatoribus. At prima maiorum urbanitas pro, modus vocibus noluisse his et.</p>
-			<h5>Heading 5</h5>
-			<p>Lorem ipsum dolor sit amet, ei justo partem vivendum eum, ad mel utinam lucilius adolescens. No solet facete perfecto ius, tibique percipitur ei eos. Cu mel nullam suscipiantur, at est link vituperata. Sed ne eius omnis suscipit, vix ex alia assueverit vituperatoribus. At prima maiorum urbanitas pro, modus vocibus noluisse his et.</p>
-			<h6>Heading 6</h6>
-			<p>Lorem ipsum dolor sit amet, ei justo partem vivendum eum, ad mel utinam lucilius adolescens. No solet facete perfecto ius, tibique percipitur ei eos. Cu mel nullam suscipiantur, at est link vituperata. Sed ne eius omnis suscipit, vix ex alia assueverit vituperatoribus. At prima maiorum urbanitas pro, modus vocibus noluisse his et.</p>
-		</div>
+		<main class="main-content"><?php
+			// start the loop
+			if (have_posts()) {
+				while (have_posts()) {
+					the_post();
+					the_content();
+				}
+			}
+		?></main>
 	</div>
 </div>
 <?php
