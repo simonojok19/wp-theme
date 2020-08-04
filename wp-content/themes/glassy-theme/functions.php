ee1c1813d587b97ee3c6315e1a524bbb397b70a8
simonojok19 <simonojok19@gmail.com>
2020-08-04 22:29:03 +0300

    added functions

--- wp-content/themes/glassy-theme/functions.php
+++ wp-content/themes/glassy-theme/functions.php
@@ -46,6 +46,30 @@ function glass_theme_widget_init() {
 
 add_action('widgets_init', 'glass_theme_widget_init');
 
-function glass_theme_card() {
+function my_user_vote() {
+	if (!wp_verify_nonce($_REQUEST['nonce'], "my_user_vote_nonce")) {
+		exit("No naughty business please");
+	}
+	$vote_count = get_post_meta($_REQUEST["post_id"], "votes", true);
+	$vote_count = ($vote_count == '')? 0 : $vote_count;
+	$new_vote_count = $vote_count + 1;
+	$vote = update_post_meta($_REQUEST["post_id"], "votes", $new_vote_count);
+	if ($vote === false) {
+		$result['type'] = "error";
+		$result["vote_count"] = $new_vote_count;
+	}
 
+	if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+		$result = json_encode($result);
+		echo $result;
+	} else {
+		header("Location: ".$_SERVER["HTTP_REFERER"]);
+	}
+
+	die();
+}
+
+function my_must_login() {
+	echo "You must log in to vote";
+	die();
 }
