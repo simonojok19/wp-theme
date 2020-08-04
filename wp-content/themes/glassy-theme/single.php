c7c8de5c4d8f614d975c13210915a0469abc6b29
simonojok19 <simonojok19@gmail.com>
2020-08-04 21:26:53 +0300

    added some numbers

---
+++ wp-content/themes/glassy-theme/single.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * Template Name: Classes Page Template
+ * Classes Page
+ */
+get_header();
+?>
+<div class="row" style="width: 80%; margin-left: auto; margin-right: auto">
+    <div class="col-xs-12 col-sm-8">
+        <?php
+            if (have_posts()) {
+                while (have_posts()) {
+                    the_post();
+                    the_title();
+                    the_content();
+                    if (has_post_thumbnail()) {
+                        the_post_thumbnail();
+                    }
+                    the_category();
+                    the_comment();
+                }
+            }
+        ?>
+    </div>
+</div>
+<?php
+get_footer();
+?>
