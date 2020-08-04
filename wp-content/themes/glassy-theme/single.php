1d95461e4aa75a5dec16c932db48e537c1eacbd5
simonojok19 <simonojok19@gmail.com>
2020-08-04 21:17:50 +0300

    adding single.php

--- wp-content/themes/glassy-theme/single.php
+++ wp-content/themes/glassy-theme/single.php
@@ -1,17 +1,19 @@
 <?php
 get_header(); ?>
 <div id="primary" class="content-area">
+    <h1>Single PHP</h1>
 	<main id="main" class="site-main" role="main">
 		<?php
 			while (have_posts()) : the_post();
-			get_template_part('content', get_post_format());
-			if (comments_open() || get_comments_number()):
-				comments_template();
-			endif;
-			the_post_navigation(array(
-				'next_text' => '<span class="meta-nav" aria-hidden="true"',
-
-			));
+//			the_post();
+//			the_content();
+//			if (comments_open() || get_comments_number()):
+//				comments_template();
+//			endif;
+//			the_post_navigation(array(
+//				'next_text' => '<span class="meta-nav" aria-hidden="true"',
+//
+//			));
 			endwhile;
 		?>
 	</main>
