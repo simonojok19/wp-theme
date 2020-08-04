7873bee4de5b4239f512ecb19225c7e21c5cd8cd
simonojok19 <simonojok19@gmail.com>
2020-08-04 11:52:22 +0300

    added some codes

--- wp-content/plugins/contact-form/contact-form.php
+++ wp-content/plugins/contact-form/contact-form.php
@@ -8,74 +8,84 @@
  * Author URI: https://github.com/simonojok19
  */
 
-function registration_form($message, $email) {
+function registration_form() {
+	$admin_email = get_option( 'admin_email' );
+	$email ='';
+	$message = '';
 	echo '
+		<script>
+			window.addEventListener("load", () => {
+			    var email = document.querySelector("#email");
+				var message = document.querySelector("#message");
+				var submit = document.querySelector("#send");
+				var adminemail = document.querySelector("#hidden");
+				submit.addEventListener("click", (event) => {
+				    event.preventDefault();
+				    email_txt = email.value;
+				    message_text = message.value;
+				    addmin_email = adminemail.value;
+				    if (Email) {
+				        Email.send({
+						    Host : "smtp.zoho.com",
+						    Username : "accounts@techbuzzhub.org",
+						    Password : "First@001",
+						    To : email_txt,
+						    From : addmin_email,
+						    Subject : "Message From The Website",
+						    Body : message_text
+						    }).then(
+						      message => alert(message)
+						 );
+				    }
+				})
+			}, false)
+		</script>
 		<style>
+			form {
+				display: flex;
+				flex-direction: column;
+			}
 			div {
 			margin-bottom: 2px;
 			}
 			
 			input {
 				margin-bottom: 4px;
+				width: 100%;
+			}
+			
+			textarea {
+				width: 100%;
+			}
+			
+			#hidden {
+				visibility: hidden;
 			}
 		</style>
 	';
 
+	echo '<input id="hidden" value="'. $admin_email.'"/>';
 	echo '
-		<form action="'.$_SERVER['REQUEST_URI'].'" method="POST">
+		<form action="" method="POST">
 			<div>
 				<label for="email">Email</label>
-				<input type="text" name="email" value="'.(isset($_POST['email']) ? $email :null ).'" placeholder="Your Email Address">
+				<br/>
+				<input type="text" id="email" name="email" value="" placeholder="Your Email Address">
 			</div>
 			<div>
 				<label for="message">Message</label>
-				<textarea rows="10" cols="20" value="'.(isset($_POST['message']) ? $message : null).'" placeholder="Your Message"/>
+				<br/>
+				<textarea id="message" rows="10" cols="20" value="" placeholder="Your Message"></textarea>
 			</div>
-			<input type="submit" name="submit" value="Send">
+			<button type="submit" id="send">Send</button>
 		</form>
 	';
 }
 
-function registration_validation($email, $message) {
-	global $contact_error;
-	$contact_error = new WP_Error();
-	if (empty($email) || empty($message)) {
-		$contact_error -> add('Email and Message are required');
-	}
 
-	if (is_wp_error($contact_error)) {
-		foreach ($contact_error -> get_error_messages() as $error) {
-			echo '
-				<div>
-					<strong>Error</strong>
-					<br/>
-				</div>
-			';
-		}
-	}
+function loadJavaScripts() {
+//	echo "<h1>loaded</h1>";
+	wp_enqueue_script('contact-form', 'https://smtpjs.com/v3/smtp.js');
 }
-
-function complete_registration() {
-	global  $contact_error, $email, $message;
-	if ( 1 > count($contact_error -> get_error_messages())) {
-		// send email
-		echo 'Email was sent successfully';
-	}
-}
-
-function custom_registration_function() {
-	if (isset($_POST['submit'])) {
-		registration_validation(
-			$_POST['email'],
-			$_POST['message']
-		);
-		global  $email, $message;
-		$email = sanitize_email($_POST['email']);
-		$message = esc_textarea($_POST['message']);
-
-		complete_registration();
-		registration_form($email, $message);
-	}
-}
-
-add_shortcode('cr_custom_registration', '');
+add_action('wp_enqueue_scripts', 'loadJavaScripts');
+add_shortcode('cr_custom_registration', 'registration_form');
