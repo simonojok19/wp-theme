f655910dd390c396c72d2e1a1eb839b11fe86223
simonojok19 <simonojok19@gmail.com>
2020-08-04 21:54:42 +0300

    adding some data

--- wp-content/themes/glassy-theme/single.php
+++ wp-content/themes/glassy-theme/single.php
@@ -7,23 +7,20 @@ get_header();
 ?>
 <div class="row" style="width: 80%; margin-left: auto; margin-right: auto">
     <div class="col-xs-12 col-sm-8">
+        <?php if (have_posts()): the_post() ?>
+        <h1><?php  the_title() ?></h1>
         <?php
-            if (have_posts()) {
-                while (have_posts()) {
-                    the_post();
-                    the_title();
-                    the_content();
-                    if (has_post_thumbnail()) {
-                        the_post_thumbnail();
-                    }
-                    the_category();
-                    the_tags();
-                    the_author_posts();
-                    the_comment();
-                    edit_post_link();
-                }
-            }
+            $votes = get_post_meta($post -> ID, "votes", true);
+            $votes = ($votes == "") ? 0 :$votes;
         ?>
+        <h3>This post has <small><?php echo $votes ?></small></h3>
+        <?php
+        $nonce = wp_create_nonce("my_user_vote_nonce");
+        $link = admin_url('admin-ajax.php?action=my_user_vote&post_id='.$post -> ID.'&nonce='.$nonce);
+        echo '<a data-nonce="'.$nonce.'" data-post_id="'.$post -> ID.'" href="'.$link.'"> vote for this article</a>'
+            ?>
+        <p><?php the_content() ?></p>
+        <?php endif; ?>
     </div>
 </div>
 <?php
